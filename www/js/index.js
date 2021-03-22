49227cb0a29b58f8bab45d8d8fd3cf4435d0a93e
nara58 <80908498+nara58@users.noreply.github.com>
2021-03-22 21:03:07 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=f0HOvQ7TNwEp94qF4f8AOh03uBv5HjQ2jcCue0k4";
         
         $("#loadImage").click(function(){
             $.ajax({
